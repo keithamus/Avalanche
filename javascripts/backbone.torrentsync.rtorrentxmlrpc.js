a4e2ce3a6938434d6f5dfed689b78cc664f6087f
Keith Cirkel <github@keithcirkel.co.uk>
2011-08-29 20:12:00 +0100

    (Torrent Sync Engine) Begin the work on rTorrent XMLRPC syncing

---
+++ javascripts/backbone.torrentsync.rtorrentxmlrpc.js
@@ -0,0 +1,161 @@
+/*jslint white: true, browser: true, devel: true, onevar: true, undef: true,
+ nomen: false, eqeqeq: true, plusplus: false, bitwise: true, regexp: true,
+ newcap: true, immed: true, maxlen: 80, indent: 4 */
+/*globals
+    $: false,
+    _: false,
+    Backbone: false,
+    window: false,
+*/
+/*
+ * lib/backbone.torrentsync.rtorrentxmlrpc.js
+ *
+ * Avalanche Project (http://avalanche-bt.com)
+ *
+ * This code is licensed under the GPL3, or
+ * "GNU GENERAL PUBLIC LICENSE Version 3"
+ * For more details, see http://opensource.org/licenses/gpl-3.0.html
+ *
+ * @author Keith Cirkel ('keithamus') <avalanche@keithcirkel.co.uk>
+ * @license http://opensource.org/licenses/gpl-3.0.html
+ * @copyright Copyright © 2011, Keith Cirkel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+(function () {
+    var SyncEngine = function () {
+        var self = this;
+        return function (args) {
+            return self.init.apply(self, Array.prototype.slice.call(arguments));
+        };
+    };
+    
+    SyncEngine.prototype = {
+        
+        templates: {
+            message:
+                '<?xml version="1.0"?>' +
+                '<methodCall>' +
+                    '<methodName>{{command}}</methodName>' +
+                    '<params>' +
+                        '{{params}}' +
+                    '</params>' +
+                '</methodCall>',
+                
+            param:
+                '<param><value><{{type}}>{{param}}</{{type}}></value></param>'
+        },
+        
+        init: function (methodName, model, options) {
+            
+            methodName = model instanceof Backbone.Collection ?
+                methodName + 'Collection' :
+                methodName;
+                
+            this.model = model;
+            this.options = options;
+            
+            $.debug('Attempting to call syncEngine.' + methodName);
+            
+            if (this[methodName]) {
+                $.debug(methodName, options);
+                return this[methodName]();
+            } else {
+                throw new Error('syncEngine has no method ' + methodName);
+            }
+            return false;
+        },
+        
+        _sendMessage: function (command, values, callback) {
+            $.ajax({
+                type: 'POST',
+                dataType: 'xml',
+                url: this.model.url,
+                data: this._formatXMLRPCMsg(command, values),
+                success: _.bind(function (data) {
+                    data = $(data);
+                    $.debug('Received message from server', data.get(0));
+                    if ($(data).find('fault').length > 0) {
+                        var fault = $(data).find('fault string').text();
+                        this.model.trigger('error', fault);
+                    }
+                    if (callback) {
+                        callback(data);
+                    }
+                }, this)
+            });
+        },
+        
+        _formatXMLRPCMsg: function (command, values) {
+            var params = '';
+            
+            _.each(values, function (value) {
+                var type = typeof value === "number" ? 'i8' : 'string';
+                params += $.tmpl(this.templates.param, {
+                    param: value,
+                    type: type
+                });
+            }, this);
+            
+            $.debug($.tmpl(this.templates.message, {
+                command: command,
+                params: params
+            }));
+            
+            return $.tmpl(this.templates.message, {
+                command: command,
+                params: params
+            });
+        },
+        
+        read: function () {
+            
+        },
+        
+        readCollection: function () {
+            this._sendMessage('d.multicall', [
+                'name', //Make an array for all loaded torrents
+                'd.get_hash=',  //The torrent hash
+                'd.get_name=', //Torrent's name
+                'd.get_state=',  //0 = stopped, 1 = started
+                'd.get_up_total=',  //How much in total has been uploaded
+                'd.get_down_rate=',  //Download rate in bytes
+                'd.get_up_rate=',  //Upload rate in bytes
+                'd.get_peers_connected=',  //Amount of connected peers
+                'd.get_peers_not_connected=',  //Amount of unconnected peers
+                'd.get_peers_accounted=',  //Number of leechers
+                'd.get_complete=',  //Is the torrent completely downloaded?
+                'd.is_hash_checking=',  //Is it rehashing?
+                'd.get_creation_date=',  //Date torrent added
+                'd.get_base_path=',  //Where the torrent exists
+                'd.get_free_diskspace=',  //Free disk space where torrent is
+                'd.is_private=',  //Is torrent private?
+                'd.get_message=',  //Comment
+                'd.get_priority=',  //Priority (number)
+                'd.is_hash_checked=',  //Has it been hash checked before?
+                'd.get_skip_total=',  //How many wasted bytes?
+                'd.get_custom5=',  //Extra torrent meta JSON
+                //http://libtorrent.rakshasa.no/ticket/1538
+                //dont use get_size_bytes, multiply chunk_size by size_chunks
+                'd.get_chunk_size=',  //Get the size of a single chunk in bytes
+                'd.get_size_chunks=',  //Get how many chunks are in the torrent
+                'd.get_completed_chunks=' //Get how many chunks have downloaded.
+            ]);
+        }
+        
+    };
+    
+    Backbone.torrentSyncRtorrentXMLRPC = new SyncEngine();
+    window.SyncEngine = SyncEngine;
+}(window));
